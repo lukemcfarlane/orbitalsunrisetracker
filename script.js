34f5732e0225774245ad45b7cbd8ea2a84cabd5e
Luke McFarlane <luke.mcfarlane@trineo.co.nz>
2014-05-13 09:03:14 +1200

    Bootstrap style, general improvements

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 // Code goes here
 
-function issTrackerCtrl($scope, $interval) {
+var app = angular.module('OrbitalSunriseTracker', function() {});
+app.controller('mainCtrl', function($scope, $interval) {
 	$scope.isEmbed = false;
+	$scope.events = [];
 
 	var updateRemainingTimes = function() {
 		var remainingTime = "ERR";
@@ -16,6 +18,10 @@ function issTrackerCtrl($scope, $interval) {
 		for (var i = 1; i < _.size(orbit.orbitData); i++) {
 			currOrbitData = orbit.orbitData[i];
 
+			if (currOrbitData.t < currTime) {
+				$scope.currentlyLit = currOrbitData.s;
+			}
+
 			if (prevOrbitData.s === false && currOrbitData.s === true) {
 				nextSunriseTime = prevOrbitData.t;
 			}
@@ -26,9 +32,35 @@ function issTrackerCtrl($scope, $interval) {
 			prevOrbitData = currOrbitData;
 		}
 
-		$scope.minsUntilSunrise = (nextSunriseTime - currTime) / 60;
-		$scope.minsUntilSunset = (nextSunsetTime - currTime) / 60;
+		$scope.events = [];
+
+		$scope.events.push({
+			type: 'sunrise',
+			timeFormatted: moment().add('minutes', (nextSunriseTime - currTime) / 60).format('hh:MM a'),
+			minsUntil: (nextSunriseTime - currTime) / 60
+		});
+
+		$scope.events.push({
+			type: 'sunset',
+			timeFormatted: moment().add('minutes', (nextSunsetTime - currTime) / 60).format('hh:MM a'),
+			minsUntil: (nextSunsetTime - currTime) / 60
+		});
 	};
 
-	$interval(updateRemainingTimes, 1000);
-}
+	$interval(updateRemainingTimes, 30000);
+	updateRemainingTimes();
+});
+app.directive('event', function() {
+	return {
+		restrict: 'E',
+		template: '<div class="panel panel-info">' +
+			'<div class="panel-heading">' +
+			'    <span class="glyphicon glyphicon-time"/>' +
+			'    {{ event.timeFormatted }}' +
+			'</div>' +
+			'<div class="panel-body">' +
+			'    The next {{ event.type }} will occur in {{ event.minsUntil | number : 0 }} minutes.' +
+			'</div>' +
+			'</div>'
+	};
+});
